83483cc7572ab19e05df687b9b86a0dee7598caa
tungdt <duongthanhtungvn01@gmail.com>
2020-01-02 09:35:02 +0700

    Update

--- face_descriptor.py
+++ face_descriptor.py
@@ -13,7 +13,7 @@ def prewhiten(x):
     mean = np.mean(x)
     std = np.std(x)
     std_adj = np.maximum(std, 1.0/np.sqrt(x.size))
-    y = np.multiply(np.subtract(x, mean), 1/std_adj)
+    y = (x-mean)/std_adj
     return y 
 
 class FaceDescriptor():
