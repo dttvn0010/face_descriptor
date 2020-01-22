cd274bdce5fe56778f92424f76ec91451611eeee
tungdt <duongthanhtungvn01@gmail.com>
2020-01-23 02:31:31 +0700

    Update

--- compare_facenet.py
+++ compare_facenet.py
@@ -6,7 +6,7 @@ from facenet.face_model import FaceModel
 model = FaceModel()
 
 f1 = model.getFaceFeatures(cv2.imread(sys.argv[1]))
-f1 = model.getFaceFeatures(cv2.imread(sys.argv[2]))
+f2 = model.getFaceFeatures(cv2.imread(sys.argv[2]))
 
 print('Euclide diatance:', np.sum(np.square(f1-f2)))
 print('Cosine simarlity:', np.dot(v1,v2))
